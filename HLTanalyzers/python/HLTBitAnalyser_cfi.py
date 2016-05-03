1e885440862715abe03929a5ba7bfc96c6c20899
ndaci <daci.nadir@gmail.com>
2016-05-03 14:53:58 +0200

    Add input parameters for the HLTJetMET component in the HLTBitAnalyzer.

--- HLTanalyzers/python/HLTBitAnalyser_cfi.py
+++ HLTanalyzers/python/HLTBitAnalyser_cfi.py
@@ -2,14 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 hltbitanalysis = cms.EDAnalyzer("HLTBitAnalyzer",
     ### Trigger objects
-    l1GctHFBitCounts                = cms.InputTag("hltGctDigis"),
-    l1GctHFRingSums                 = cms.InputTag("hltGctDigis"),
-    l1GtObjectMapRecord             = cms.InputTag("hltL1GtObjectMap::HLT"),
-    l1results                       = cms.InputTag("hltGtDigis::HLT"),                            
-##    l1GtReadoutRecord               = cms.InputTag("hltGtDigis::HLT"),
+    #l1GctHFBitCounts                = cms.InputTag("hltGctDigis"),
+    #l1GctHFRingSums                 = cms.InputTag("hltGctDigis"),
+    #l1GtObjectMapRecord             = cms.InputTag("hltL1GtObjectMap::HLT"),
+    l1results                       = cms.InputTag("hltGtStage2Digis"),                            
+    #l1GtReadoutRecord               = cms.InputTag("hltGtDigis::HLT"),
 
-    l1extramc                       = cms.string('hltL1extraParticles'),
-    l1extramu                       = cms.string('hltL1extraParticles'),
+    #l1extramc                       = cms.string('hltL1extraParticles'),
+    #l1extramu                       = cms.string('hltL1extraParticles'),
     hltresults                      = cms.InputTag("TriggerResults::HLT"),
     HLTProcessName                  = cms.string("HLT"),
 
@@ -22,14 +22,40 @@ hltbitanalysis = cms.EDAnalyzer("HLTBitAnalyzer",
                                 
     ## reco vertices
     OfflinePrimaryVertices0     = cms.InputTag('offlinePrimaryVertices'),
+
+    ## HLT collections //ND
+    hltPFJetLabel               = cms.InputTag('hltAK4PFJets::TEST'),
+    hltPFJetCorrLabel           = cms.InputTag('hltAK4PFJetsCorrected::TEST'),
+    hltCaloJetLabel             = cms.InputTag('hltAK4CaloJets::TEST'),
+    hltCaloJetCorrLabel         = cms.InputTag('hltAK4CaloJetsCorrected::TEST'),
+    hltPFMETLabel               = cms.InputTag('hltPFMETProducer::TEST'), 
+    hltPFMETNoMuLabel           = cms.InputTag('hltPFMETNoMuProducer::TEST'), 
+    hltPFMHTLabel               = cms.InputTag('hltPFMHTTightID::TEST'), 
+    hltPFMHTNoMuLabel           = cms.InputTag('hltPFMHTNoMuTightID::TEST'), 
+    hltCaloMETLabel             = cms.InputTag('hltMet::TEST'), 
+    hltCaloMETCleanLabel        = cms.InputTag('hltMetClean::TEST'), 
+    hltCaloMHTLabel             = cms.InputTag('hltMht::TEST'),
+
+    ## RECO collections //ND
+    recoPFJetLabel               = cms.InputTag('ak4PFJets::RECO'),
+    recoPFJetCorrLabel           = cms.InputTag('ak4PFJetsCHS::RECO'),
+    recoCaloJetLabel             = cms.InputTag('ak4CaloJets::RECO'),
+    recoCaloJetCorrLabel         = cms.InputTag('ak4CaloJets::RECO'),
+    recoPFMETLabel               = cms.InputTag('pfMet::RECO'), 
+    recoMuonsLabel               = cms.InputTag('muons::RECO'),
+    recoCaloMETLabel             = cms.InputTag('caloMet::RECO'), 
                                 
     ### Run parameters
     RunParameters = cms.PSet(
         HistogramFile = cms.untracked.string('hltbitanalysis.root'),
         isData         = cms.untracked.bool(False),
         Monte          = cms.bool(True),
-        GenTracks      = cms.bool(True)
-
+        GenTracks      = cms.bool(True),
+        RecoPFJets     = cms.bool(False),
+        RecoCaloJets   = cms.bool(False),
+        RecoPFMET      = cms.bool(False),
+        RecoCaloMET    = cms.bool(False),
+        RecoMuons      = cms.bool(False)
     )
                                 
 )
